136723413a5d75ce11de75886e63cd18bf499db4
justin <justinchiu@fstop.com.tw>
2022-03-08 17:43:10 +0800

    3/8 17:42

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 2/22 start to develop project and try to learn the layout about node
 2/25 posting is successsful as for get i want to create a list from database before the api  get start so i can judge persently 
+3/8 解決了 網頁url帶id會redirect到新頁面 這個步驟所出現的error跟exception 接下來要研究redis讓指定id的資料先存起來
---
+++ requestHandler.js
@@ -0,0 +1,105 @@
+const express = require("express")
+const app =express();
+const mysql = require("mysql");
+const bodyParser = require('body-parser');
+app.use(bodyParser.text({type: '*/*'}))
+const redis = require("redis")
+const url = require("url")
+const sd = require('silly-datetime');
+const client = redis.createClient();
+
+client.on('connect',function(){
+    console.log('Connected to Redis');
+});
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'localDb'
+});
+
+function post_action(){
+    app.post("/api/v1/urls",(req,res)=>{
+        console.log("start posting")
+        var user = JSON.parse(req.body).url
+        console.log(user)
+        var expireAt = JSON.parse(req.body).expireAt
+        connection.query('select count(*) "count" from urlDb',function(err,result){
+            if(err) throw err
+            const count = result[0].count
+            const shortUrl = user+"/"+count;
+            connection.query("insert into urlDb(url,shortUrl,expired_time) values ('"+user+"','"+shortUrl+"','"+expireAt+"');",function(err,result){
+            if(err) throw err
+            res.send("id:"+count+","+"\r\n"+"shortUrl:  "+"http://localhost/"+shortUrl)
+            });
+        });
+    });
+}
+
+function findById_action(){
+    app.get("/:id",(req,res,next)=>{
+        let id = url.parse(req.url).pathname;
+        id = id.substring(1)
+        let expired_time
+        let redir =""
+        connection.query('select expired_time,url "redirUrl" from urlDb where id = ?',[id],(err,result)=>{
+            if(err){
+                console.log("findbyid occur error")
+            }else{
+                if(isObjEmpty(result)){
+                    // client.setEx("url",result[0].redirUrl);
+                    // console.log("redis: "+client.get("url"))
+                    res.writeHead(404, {"Content-Type": "text/plain"});
+                    res.write("Can't find the id in db");
+                    res.send()
+                    next()
+                }else{
+                    redir = result[0].redirUrl
+                    expired_time =  new Date(result[0].expired_time)
+                    if(checkTime(expired_time)){
+                        res.writeHead(404, {"Content-Type": "text/plain"});
+                        res.write("time has already expired");
+                        res.send()
+                        next()
+                    }else{
+                        res.redirect("http://localhost:8888/"+redir+"")
+                        next()
+                    }
+                }
+            }
+        })
+    });
+}
+
+function findByRedis(){
+    app.get('/:id',(req,res)=>{
+        let id = url.parse(req.url).pathname;
+        id = id.substring(1)
+        connection.query('selec expired_time,url "redirUrl" from urlDb where id = ?',[id],(err,result)=>{
+            client.setEx("url",result[0].redirUrl);
+            console.log(client.get("url"))
+        })
+    })
+}
+
+function isObjEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+function checkTime(expir){
+    var presentTime = sd.format(new Date(),'YYYY-MM-DD HH:mm:ss');
+    presentTime = new Date(presentTime)
+    if(presentTime>expir){
+        return true
+    }else{
+        return false
+    }
+}
+
+app.listen(8888,()=>
+    console.log("Server 8888 is start")
+)
+
+exports.post_action = post_action
+exports.findById_action  = findById_action
